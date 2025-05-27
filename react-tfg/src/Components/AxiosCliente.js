4ab37907b80ce72f0b833a0878cb518815908b97
Hugo Novo Presumido <hugonovopresu@gmail.com>
2025-05-27 16:56:23 +0200

    qwe

--- react-tfg/src/Components/AxiosCliente.js
+++ react-tfg/src/Components/AxiosCliente.js
@@ -1,15 +1,13 @@
-
 import axios from 'axios';
 
 const axiosCliente = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api',
+  baseURL: 'https://tfg-bacl-production.up.railway.app/api',
   headers: {
     'Content-Type': 'application/json',
     Accept: 'application/json',
   }
 });
 
-// Interceptor para añadir el token automáticamente
 axiosCliente.interceptors.request.use(config => {
   const usuario = JSON.parse(localStorage.getItem('usuario'));
   if (usuario?.token) {
